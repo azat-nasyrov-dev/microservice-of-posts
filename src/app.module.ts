b07b57c48aa95054ef5584044919a3785fb7e659
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-10-28 06:30:43 +0600

    added all exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ProvidersModule } from '@lib/providers';
+import { SharedModule } from '@lib/shared';
 
 @Module({
-  imports: [ProvidersModule],
+  imports: [SharedModule, ProvidersModule],
 })
 export class AppModule {}
