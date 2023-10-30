3b26c9ea7c09f2635b4dff6f90e7ed872bd6a0c4
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-10-31 01:06:29 +0600

    updated facade. Added commands and queries

--- libs/post/src/application-services/commands/index.ts
+++ libs/post/src/application-services/commands/index.ts
@@ -5,6 +5,18 @@ import { UpdatePostCommandHandler } from '@lib/post/application-services/command
 import { DeletePostCommandHandler } from '@lib/post/application-services/commands/delete-post/delete-post.command-handler';
 import { SetPublishedCommandHandler } from '@lib/post/application-services/commands/set-published/set-published.command-handler';
 
+// commands
+export * from './create-post/create-post.command';
+export * from './delete-post/delete-post.command';
+export * from './set-published/set-published.command';
+export * from './update-post/update-post.command';
+
+// command-handlers
+export * from './create-post/create-post.command-handler';
+export * from './delete-post/delete-post.command-handler';
+export * from './set-published/set-published.command-handler';
+export * from './update-post/update-post.command-handler';
+
 export const POST_COMMANDS_HANDLERS: Type<ICommandHandler>[] = [
   CreatePostCommandHandler,
   UpdatePostCommandHandler,
--- libs/post/src/application-services/post.facade.ts
+++ libs/post/src/application-services/post.facade.ts
@@ -1,5 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CommandBus, EventBus, QueryBus } from '@nestjs/cqrs';
+import {
+  CreatePostDto,
+  UpdatePostDto,
+} from '@lib/post/application-services/commands/dto';
+import {
+  CreatePostCommand,
+  CreatePostCommandHandler,
+  DeletePostCommand,
+  DeletePostCommandHandler,
+  SetPublishedCommand,
+  SetPublishedCommandHandler,
+  UpdatePostCommand,
+  UpdatePostCommandHandler,
+} from '@lib/post/application-services/commands';
+import {
+  GetPostQuery,
+  GetPostQueryHandler,
+  GetPostsQuery,
+  GetPostsQueryHandler,
+} from '@lib/post/application-services/queries';
+import { PaginationDto } from '@lib/shared/dto';
 
 @Injectable()
 export class PostFacade {
@@ -9,7 +30,58 @@ export class PostFacade {
     private readonly eventBus: EventBus,
   ) {}
 
-  commands = {};
-  queries = {};
+  commands = {
+    createPost: (post: CreatePostDto) => this.createPost(post),
+    deletePost: (id: string) => this.deletePost(id),
+    setPublished: (id: string) => this.setPublishedPost(id),
+    updatePost: (post: UpdatePostDto) => this.updatePost(post),
+  };
+
+  queries = {
+    getOnePost: (id: string) => this.getPost(id),
+    getAllPosts: (pagination: PaginationDto) => this.getPosts(pagination),
+  };
+
   events = {};
+
+  private createPost(post: CreatePostDto) {
+    return this.commandBus.execute<
+      CreatePostCommand,
+      CreatePostCommandHandler['execute']
+    >(new CreatePostCommand(post));
+  }
+
+  private deletePost(id: string) {
+    return this.commandBus.execute<
+      DeletePostCommand,
+      DeletePostCommandHandler['execute']
+    >(new DeletePostCommand(id));
+  }
+
+  private setPublishedPost(id: string) {
+    return this.commandBus.execute<
+      SetPublishedCommand,
+      SetPublishedCommandHandler['execute']
+    >(new SetPublishedCommand(id));
+  }
+
+  private updatePost(post: UpdatePostDto) {
+    return this.commandBus.execute<
+      UpdatePostCommand,
+      UpdatePostCommandHandler['execute']
+    >(new UpdatePostCommand(post));
+  }
+
+  private getPost(id: string) {
+    return this.queryBus.execute<GetPostQuery, GetPostQueryHandler['execute']>(
+      new GetPostQuery(id),
+    );
+  }
+
+  private getPosts(pagination: PaginationDto) {
+    return this.queryBus.execute<
+      GetPostsQuery,
+      GetPostsQueryHandler['execute']
+    >(new GetPostsQuery(pagination));
+  }
 }
