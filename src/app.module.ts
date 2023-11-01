974a8dced748340038ab1834f21ce343b735b370
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-11-01 10:13:01 +0600

    added controllers | post controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProvidersModule } from '@lib/providers';
 import { SharedModule } from '@lib/shared';
+import { ApiModule } from './api';
 
 @Module({
-  imports: [SharedModule, ProvidersModule],
+  imports: [SharedModule, ProvidersModule, ApiModule],
 })
 export class AppModule {}
