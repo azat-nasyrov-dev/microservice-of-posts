850df9a0a970254467b991470bcfc03d6b41ff6e
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-11-02 22:46:48 +0600

    added post method: create post

--- src/api/controllers/post/post.controller.ts
+++ src/api/controllers/post/post.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { PostFacade } from '@lib/post/application-services';
+import { CreatePostDto } from './dto';
+import { CurrentUser, ICurrentUser } from '@lib/auth';
+import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+  constructor(private readonly postFacade: PostFacade) {}
+
+  @Post()
+  createPost(
+    // @CurrentUser() user: ICurrentUser,
+    @Body() createPostDto: CreatePostDto,
+  ) {
+    return this.postFacade.commands.createPost({
+      ...createPostDto,
+      authorId: randomStringGenerator(),
+      // authorId: user.userId,
+    });
+  }
+}
