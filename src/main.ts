9477e8a3ea060885ea189a26f616cff4dd0f37ce
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-11-15 03:21:43 +0600

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Posts')
+    .setDescription('CRUD API by posts')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+
   await app.listen(3000);
 }
 
