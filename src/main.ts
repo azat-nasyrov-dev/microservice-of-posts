1dff27674c0b4de6300c22c2983e17a337ff7864
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-11-15 03:28:29 +0600

    added log

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
 
   // Swagger
   const config = new DocumentBuilder()
@@ -14,7 +18,13 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-doc', app, document);
 
-  await app.listen(3000);
+  await app.listen(port, () => {
+    Logger.log(`Application started on http://localhost:${port}`, 'Main');
+    Logger.log(
+      `Swagger documentation on http://localhost:${port}/api-doc`,
+      'Main',
+    );
+  });
 }
 
 bootstrap();
