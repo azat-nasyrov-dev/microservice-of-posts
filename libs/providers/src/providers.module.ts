8b981bf13bd6668e21b798d561fbc01b200af588
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-11-17 10:18:06 +0600

    added graphql module

--- libs/providers/src/providers.module.ts
+++ libs/providers/src/providers.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeormModule } from './typeorm/typeorm.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { apolloDriverConfig } from '@lib/providers/graphql';
 
 @Module({
-  imports: [TypeormModule],
+  imports: [
+    TypeormModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>(apolloDriverConfig),
+  ],
 })
 export class ProvidersModule {}
